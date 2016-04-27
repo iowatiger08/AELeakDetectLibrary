b1aa7483a3139399c8b346c94d5c1cb203933b25
Tony Dillon-Hansen <tdillon-hansen@sourceallies.com>
2016-04-27 13:46:35 -0500

    update AETestResults and valves

--- src/main/java/com/sourceallies/aeleakdetection/model/AETestResults.java
+++ src/main/java/com/sourceallies/aeleakdetection/model/AETestResults.java
@@ -2,4 +2,41 @@ package com.sourceallies.aeleakdetection.model;
 
 public class AETestResults {
 
+    private Boolean coupling;
+    private Integer grade;
+    private Integer noise;
+    private Double decibelLevel;
+    private Double flowTime;
+    
+    public Boolean getCoupling() {
+        return coupling;
+    }
+    public void setCoupling(Boolean coupling) {
+        this.coupling = coupling;
+    }
+    public Integer getGrade() {
+        return grade;
+    }
+    public void setGrade(Integer grade) {
+        this.grade = grade;
+    }
+    public Integer getNoise() {
+        return noise;
+    }
+    public void setNoise(Integer noise) {
+        this.noise = noise;
+    }
+    public Double getDecibelLevel() {
+        return decibelLevel;
+    }
+    public void setDecibelLevel(Double decibelLevel) {
+        this.decibelLevel = decibelLevel;
+    }
+    public Double getFlowTime() {
+        return flowTime;
+    }
+    public void setFlowTime(Double flowTime) {
+        this.flowTime = flowTime;
+    }
+    
 }
--- src/main/java/com/sourceallies/aeleakdetection/model/Valve.java
+++ src/main/java/com/sourceallies/aeleakdetection/model/Valve.java
@@ -3,8 +3,27 @@ package com.sourceallies.aeleakdetection.model;
 public abstract class Valve {
 
     private String type ;
+    private String aeModel;
+    private String fluid;
     
     public String getType(){
         return type;
     }
+
+    public String getAeModel() {
+        return aeModel;
+    }
+
+    public void setAeModel(String aeModel) {
+        this.aeModel = aeModel;
+    }
+
+    public String getFluid() {
+        return fluid;
+    }
+
+    public void setFluid(String fluid) {
+        this.fluid = fluid;
+    }
+    
 }
